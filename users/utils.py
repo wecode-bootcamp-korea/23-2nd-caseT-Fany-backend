b363cb83ce49c75555d66075a8279f4a94e4ed00
김훈태 <hoontae.kim@Hoontaes-MacBookPro.local>
2021-08-19 16:56:14 +0900

    Add: DECORATOR 추가

---
+++ users/utils.py
@@ -0,0 +1,25 @@
+import jwt
+
+from django.http            import JsonResponse
+from django.core.exceptions import ObjectDoesNotExist
+
+from users.models           import User
+from my_settings            import SECRET_KEY
+
+def login_decorator(func):
+    def wrapper(self,request, *args, **kwargs):
+        try:
+            access_token = request.headers.get('Authorization', None)
+            payload      = jwt.decode(access_token ,SECRET_KEY, algorithms= 'HS256')
+            user         = User.objects.get(id = payload['id'])
+            request.user = user
+
+        except jwt.exceptions.DecodeError:
+            return JsonResponse({'message':'INVALID_TOKEN'}, status = 400)
+        
+        except ObjectDoesNotExist:
+            return JsonResponse({'message':'INVALID_USER'}, status=400)
+
+        return func(self,request, *args, **kwargs)
+        
+    return wrapper
