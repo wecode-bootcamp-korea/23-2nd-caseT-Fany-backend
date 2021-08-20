790a695cb7eed597cf84411d00d2d27a363ff9ba
김훈태 <hoontae.kim@Hoontaes-MacBookPro.local>
2021-08-20 10:44:54 +0900

    Add:상품리스트

--- products/views.py
+++ products/views.py
@@ -3,7 +3,7 @@ import json, re, bcrypt, jwt
 from django.views           import View
 from django.http            import JsonResponse
 from django.db              import transaction
-from django.db.models       import Avg, Count, Q
+from django.db.models       import Avg, Count, Q, Sum
 
 from products.models        import Product, CustomProduct, ProductOption, Size, Color, FontColor, FontStyle, CustomImage
 
@@ -50,3 +50,60 @@ class ProductView(View):
 
         except KeyError:
             return JsonResponse({'MESSAGE':'KEY_ERROR'}, status = 400)
+
+class ProductsView(View):
+    def get(self,request):
+        category        = request.GET.get('category',None)
+        sub_category    = request.GET.get('sub_category',None)
+        detail_category = request.GET.getlist('detail_category',None)
+        color           = request.GET.getlist('color',None)
+        sorting         = request.GET.get('sorting',None)
+        search          = request.GET.get('KeyWord',None)
+        limit           = request.GET.get('limit',None)
+        offset          = int(request.GET.get('offset',0))
+    
+        filters = Q()
+
+        if category :
+            filters &= Q(detail_category__sub_category__category = category) 
+
+        if sub_category :
+            filters &= Q(detail_category__sub_category = sub_category)
+
+        if detail_category :
+            filters &= Q(detail_category_in = detail_category)
+        
+        if color :
+            filters &= Q(productoption__color__in = color)
+
+        if search :
+            filters = (Q(name__icontains = search)|
+                Q(detail_category__detail__icontains = search)|
+                Q(detail_category__sub_category__option__icontains = search)|
+                Q(detail_category__sub_category__category__name__icontains = search)
+                )
+        
+        sort_name = {
+            'new'        : '-create_at',
+            'bestseller' : '-sales',
+        }
+
+        if limit:
+            limit = offset + int(limit)
+
+        products = Product.objects.filter(filters)\
+                                  .annotate(sales = Sum('productoption__sales'))\
+                                  .order_by(sort_name.get(sorting,'id'))[offset:limit]
+
+        results = [{
+            'id'           : product.id,
+            'name'         : product.name,
+            'price'        : round(product.price),
+            'main_image'   : product.main_image,
+            'sales'        : product.sales,
+            'category'     : product.detail_category.sub_category.category.name,
+            'sub_category' : product.detail_category.sub_category.option,
+            'detail'       : product.detail_category.detail,
+            }for product in products]
+    
+        return JsonResponse({'results' : results}, status = 200)
--- users/utils.py
+++ users/utils.py
@@ -6,7 +6,8 @@ from django.core.exceptions import ObjectDoesNotExist
 from users.models           import User
 from my_settings            import SECRET_KEY
 
-def login_decorator(func):
+
+def LoginDecorator(func):
     def wrapper(self,request, *args, **kwargs):
         try:
             access_token = request.headers.get('Authorization', None)
